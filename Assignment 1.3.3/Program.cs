ec1d1541e0f04da13d80f83ed869806b70a3c664
Rahei <Rahei@RAHEIMSPC>
2024-11-14 09:37:47 -0500

    Add project files.

---
+++ Assignment 1.3.3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment_1._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+
+            int[] numbers = { 2, 5, 7 };
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            Console.WriteLine("\nNumbers stored in reverse order:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("\nNumbers stored in the original order:");
+            Array.Sort(numbers);
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+           
+        }
+    }
+}
+        
+    
+
